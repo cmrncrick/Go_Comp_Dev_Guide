b9a3d9d338a2a0e916295b812a2d580f1add3f37
cmrncrick <77648222+cmrncrick@users.noreply.github.com>
2025-04-21 08:54:20 -0500

    Section 2
    Completed

--- hello_world/main.go
+++ hello_world/main.go
@@ -1,10 +1,11 @@
-/// 
+// Used when creating your project as executable
+// Can use package blahblah to create "reusable" code, i.e. library
 package main
 
-///
-import "fmt" 
+// All native libraries at golang.org/pkg
+import "fmt"
 
-///
+// func == function (same as python def)
 func main() {
 	fmt.Println("Hi there!")
 }
